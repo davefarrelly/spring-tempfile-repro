490aab2993f98a2bdb8f07bd1a1e3b8073d013a5
davefarrelly <dfarrelly@accela.com>
2023-09-13 15:28:04 +0100

    Add reproducer

---
+++ src/main/java/com/davefarrelly/tempfile/Controlller.java
@@ -0,0 +1,28 @@
+package com.davefarrelly.tempfile;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.codec.multipart.FilePart;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+
+@RestController
+@AllArgsConstructor
+@Slf4j
+@RequestMapping("/upload")
+public class Controlller {
+
+    @PostMapping
+    public Mono<String> uploadFile(@RequestPart("uploadedFile") FilePart uploadedFile) {
+        return Mono.delay(Duration.ofSeconds(1))
+                .thenReturn(uploadedFile.filename());
+    }
+
+}
